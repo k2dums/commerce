604df9ed74f29d2380ed6aa5b58668bb536a43de
k2dums <norbu.paddu@gmail.com>
2022-09-02 21:38:38 +0530

    Listing model regiestered

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User,Listing
 
-
+class ListingAdmin(admin.ModelAdmin):
+    list_display=("name","price","time")
 
 # Register your models here.
 admin.site.register(User,UserAdmin)
+admin.site.register(Listing,ListingAdmin)
