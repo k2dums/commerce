ef4cb554438ca0a24acee3d270cad530d6afdc9f
k2dums <norbu.paddu@gmail.com>
2022-09-02 19:27:32 +0530

    added listing functiong in the views

--- auctions/views.py
+++ auctions/views.py
@@ -1,10 +1,12 @@
+from cgi import print_exception
+from unicodedata import name
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User,Listing
 
 
 def index(request):
@@ -61,3 +63,17 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+def listing(request):
+    if not(request.user.is_authenticated):
+        return render(request,"auctions/login.html",{ 
+            "message":"Need to be logged in to create Listing"
+        })
+    if request.method=="POST":
+        name=request.POST["name"]
+        price=request.POST["price"]
+        description=request.POST["description"]
+        time=request.POST["time"]
+    return render(request,"auctions/listing.html")
+\
+    
