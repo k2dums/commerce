fc61259b6cee68fd93d4221779ef1b0d3cc9c3e9
k2dums <norbu.paddu@gmail.com>
2022-09-04 23:57:17 +0530

    regiesterd Bid,Comment and Category model to the admin. Added Admin views for Bids,Comment,Category

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User,Listing
+from .models import Category, User,Listing,Bids,Comment
 
 class ListingAdmin(admin.ModelAdmin):
-    list_display=("name","price","time")
-
+    list_display=("name","price","time","user")
+class BidsAdmin(admin.ModelAdmin):
+    list_display=("name","price","user")
+class CommentAdmin(admin.ModelAdmin):
+    list_display=("name","comment","user")
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=("name")
+    
 # Register your models here.
 admin.site.register(User,UserAdmin)
 admin.site.register(Listing,ListingAdmin)
+admin.site.register(Bids,BidsAdmin)
+admin.site.register(Comment,CommentAdmin)
+admin.site.register(Category)
