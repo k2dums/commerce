fede62b48155c8f0714ce063fd7acf28592907e7
k2dums <norbu.paddu@gmail.com>
2022-09-04 23:56:06 +0530

    added sentinel functions for default category, added Category,Bid,Comment models. Listing model now has fields bid_start,category,user and its return str type changed

--- auctions/models.py
+++ auctions/models.py
@@ -1,18 +1,48 @@
+from unicodedata import category
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
+def get_sentinel_category():
+    return Category.objects.get_or_create(name="None")[0]
+def get_sentinel_category_id():
+    return get_sentinel_category().id
 
 
 class User(AbstractUser):
     pass
 
+class Category(models.Model):
+    name=models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.name}"
 class Listing(models.Model):
     name=models.CharField(max_length=64)
     price=models.IntegerField()
     description=models.CharField(max_length=64)
     time=models.CharField(max_length=64)
+    bid_start=models.IntegerField()
+    category=models.ForeignKey(Category,on_delete=models.SET(get_sentinel_category),default=get_sentinel_category_id,related_name="listing_category")
+    user=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
     
-# class Bids(models.Model):
-#    pass
-# class Comment(models.Model):
-#     pass
+    def __str__(self):
+        return f"Item-id({self.id}):Name({self.name}):Price({self.price})"
+
+    
+class Bids(models.Model):
+    name=models.ForeignKey(Listing,on_delete=models.CASCADE,related_name='bids')
+    price=models.IntegerField()
+    user=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    
+    def __str__(self):
+        return f"{self.name} BidPrice({self.price}) by {self.user}"
+
+class Comment(models.Model):
+    name=models.ForeignKey(Listing,on_delete=models.CASCADE,related_name='comments')
+    comment=models.TextField(max_length=1000)
+    user=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    
+    def __str__(self):
+        return f"Comment-id:{self.id} by {self.user}"
+
