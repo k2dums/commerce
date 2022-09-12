7253ce7e0a249285cba2c94e61c714484a25644b
k2dums <norbu.paddu@gmail.com>
2022-09-13 02:12:17 +0530

    Added many to many field for watchlist

--- auctions/models.py
+++ auctions/models.py
@@ -1,3 +1,4 @@
+from tokenize import blank_re
 from unicodedata import category
 from django.contrib.auth.models import AbstractUser
 from django.db import models
@@ -7,8 +8,6 @@ def get_sentinel_category():
     return Category.objects.get_or_create(name="None")[0]
 def get_sentinel_category_id():
     return get_sentinel_category().id
-
-
 class User(AbstractUser):
     pass
 
@@ -17,6 +16,7 @@ class Category(models.Model):
 
     def __str__(self):
         return f"{self.name}"
+
 class Listing(models.Model):
     name=models.CharField(max_length=64)
     price=models.IntegerField()
@@ -25,11 +25,10 @@ class Listing(models.Model):
     bid_start=models.IntegerField()
     category=models.ForeignKey(Category,on_delete=models.SET(get_sentinel_category),default=get_sentinel_category_id,related_name="listing_category")
     user=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
-    
+    watchlist=models.ManyToManyField(User,blank=True,related_name="users_watchList")
     def __str__(self):
         return f"Item-id({self.id}):Name({self.name}):Price({self.price})"
 
-    
 class Bids(models.Model):
     name=models.ForeignKey(Listing,on_delete=models.CASCADE,related_name='bids')
     price=models.IntegerField()
@@ -45,4 +44,5 @@ class Comment(models.Model):
     
     def __str__(self):
         return f"Comment-id:{self.id} by {self.user}"
+
 
